2c23f943f711ed09382792a6fee13bce304074f8
rhumbert1 <43270026+rhumbert1@users.noreply.github.com>
2019-06-19 09:43:44 +0200

    dropdown responsive

--- js/main.js
+++ js/main.js
@@ -4,7 +4,14 @@ $(".hover").mouseleave(
   }
 );
 
-
+function responsiveNavbar() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
 
 var txt = getElementsByName('address');
 var str = txt.split(",");
