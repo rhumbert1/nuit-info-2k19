770c772a2ad6d1fbf9378c0133a067b76adbde78
rhumbert1 <43270026+rhumbert1@users.noreply.github.com>
2019-06-20 14:51:53 +0200

    oui

--- js/main.js
+++ js/main.js
@@ -10,3 +10,14 @@ function responsiveNavbar() {
 function openUrl() {
 window.location.href = "index.html";
 }
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+$('.dropbtn').click(function () {
+  var isvisible = $(this).next('.dropdown-content').is(":visible")
+  $('.dropdown-content:visible').hide();
+  if (!isvisible)
+    {
+      $(this).next('.dropdown-content').toggle()
+    }
+})
