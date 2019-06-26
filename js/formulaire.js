4db6f120a427a95c396911f83b3b26045daa91b9
rhumbert1 <43270026+rhumbert1@users.noreply.github.com>
2019-06-26 13:46:09 +0200

    fix navbar formulaire

--- js/formulaire.js
+++ js/formulaire.js
@@ -1,3 +1,28 @@
+function responsiveNavbar() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
+
+function openUrl() {
+window.location.href = "https://rhumbert1.github.io/devis-ms/index.html";
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+$('.dropbtn').click(function () {
+  var isvisible = $(this).next('.dropdown-content').is(":visible")
+  $('.dropdown-content:visible').hide();
+  if (!isvisible)
+    {
+      $(this).next('.dropdown-content').toggle()
+    }
+})
+
+
 // Reload Page Function //
 $(document).ready(function(){
     //Check if the current URL contains '#'
