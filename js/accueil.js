c5d51df11b0abf5e1f8a366cf3905841227408f2
rhumbert1 <43270026+rhumbert1@users.noreply.github.com>
2019-06-28 08:44:24 +0200

    slideshow

--- js/accueil.js
+++ js/accueil.js
@@ -1,17 +1,17 @@
-/*var acc = document.getElementsByClassName("accordion");
-var i;
+var slideIndex = 0;
+showSlides();
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = document.getElementsByClassName("panel");
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}*/
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
 
 $('.titre-table').click(function(){
   $(this).siblings('.plan').slideToggle("slow");
